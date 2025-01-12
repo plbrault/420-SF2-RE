6242ef050a100dfaaa4e808761cb36790ef71a91
Pier-Luc Brault <pier-luc@brault.me>
2025-01-12 17:05:58 -0500

    Base structure

---
+++ solutions-labs/lab00-revision/fonctions.cpp
@@ -0,0 +1,12 @@
+#include "fonctions.h"
+
+using namespace std;
+
+void afficherMenu() {
+    cout << "Liste de tâches" << endl
+        << "================================" << endl
+        << "1. Afficher la liste" << endl
+        << "2. Ajouter une tâche" << endl
+        << "3. Marquer une tâche comme faite" << endl
+        << "4. Échanger deux tâches" << endl;
+}
