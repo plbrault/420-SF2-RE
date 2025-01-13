42091fdc820361c22f86350c9b4e44a7a98e3963
Pier-Luc Brault <pier-luc@brault.me>
2025-01-12 19:37:03 -0500

    Solution laboratoire 0 (#3)

---
+++ solutions-labs/lab00-revision/fonctions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void afficherMenu();
+int demanderChoix();
+void afficherListe(std::string taches[], bool tacheCompletee[], int nombreTaches);
+void ajouterTache(std::string taches[], int& nombreTaches);
+int demanderNumeroTache(std::string texte, int nombreTaches);
+void marquerFaite(std::string taches[], bool tacheCompletee[], int nombreTaches);
+void echangerTaches(std::string taches[], bool tacheCompletee[], int nombreTaches);
