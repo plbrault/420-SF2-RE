b8603b5c02380af433adb90121925da7a301dd80
Pier-Luc Brault <pier-luc@brault.me>
2025-03-13 20:39:11 -0400

    Script fonctionnel et carte 1 créée

--- laboratoires/lab06-vecteurs/cartes-partie2/generateur_carte.py
+++ laboratoires/lab06-vecteurs/cartes-partie2/generateur_carte.py
@@ -1,5 +1,15 @@
 import copy
 import random
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: python generateur_carte.py <rows> <cols> <output_file>")
+    sys.exit(1)
+
+rows = int(sys.argv[1])
+cols = int(sys.argv[2])
+
+output_filename = sys.argv[3]
 
 def print_matrix(matrix, cell_width=4):
     def justify(number, width):
@@ -9,41 +19,85 @@ def print_matrix(matrix, cell_width=4):
             print(justify(c, cell_width), end='')
         print()        
 
-ROWS = 200
-COLS = 30
-
-print('Generating matrix of size', ROWS, 'x', COLS)
+print('Generating matrix of size', rows, 'x', cols)
 
 matrix = []
-for i in range(ROWS):
+for i in range(rows):
     row = []
-    for j in range(COLS):
+    for j in range(cols):
         row.append(random.randint(0, 200))
     matrix.append(row)
 
 highlighted_matrix = copy.deepcopy(matrix)
+visited_positions = []
 
 NORTH = (0, -1)
 EAST = (1, 0)
 SOUTH = (0, 1)
 WEST = (-1, 0)
 
-def create_path(matrix, path_length):
-    directions = [NORTH, EAST, SOUTH, WEST]
-    pos = (random.randint(0, ROWS - 1), random.randint(0, COLS - 1))
-    matrix[pos[0]][pos[1]] = 0
-    highlighted_matrix[pos[0]][pos[1]] = '(0)'
-    previous_val = 0
+directions = [NORTH, EAST, SOUTH, WEST] 
+
+def pick_new_position(matrix, current_pos):
+    def can_use_position(pos):
+        return (
+            pos[0] >= 0
+            and pos[0] < len(matrix)
+            and pos[1] >= 0
+            and pos[1] < len(matrix[0])
+            and pos not in visited_positions
+        )
+    direction = random.choice(directions)
+    new_pos = (current_pos[0] + direction[0], current_pos[1] + direction[1])
+    tries = 1
+    while (
+        tries < 4 
+        and not can_use_position(new_pos)
+    ):
+        if direction == NORTH:
+            direction = EAST
+        elif direction == EAST:
+            direction = SOUTH
+        elif direction == SOUTH:
+            direction = WEST
+        else:
+            direction = NORTH
+        new_pos = (current_pos[0] + direction[0], current_pos[1] + direction[1])
+        tries += 1
+    if can_use_position(new_pos):
+        return new_pos
+    else:
+        return None
+
+def create_path(matrix, start_pos, path_length, continue_path=False):
+    pos = start_pos
+    if continue_path:
+        previous_val = matrix[pos[0]][pos[1]]
+    else:
+        previous_val = matrix[pos[0]][pos[1]] = 0
+        highlighted_matrix[pos[0]][pos[1]] = '(0)'
+        visited_positions.append(pos)
     for i in range(path_length):
-        direction = random.choice(directions)
-        pos = (pos[0] + direction[0], pos[1] + direction[1])
-        if pos[0] < 0 or pos[0] >= ROWS or pos[1] < 0 or pos[1] >= COLS:
-            continue
+        pos = pick_new_position(matrix, pos)
+        if pos is None:
+            break
         matrix[pos[0]][pos[1]] = previous_val + 1
         highlighted_matrix[pos[0]][pos[1]] = '(' + str(previous_val + 1) + ')'
         previous_val += 1
+        visited_positions.append(pos)
 
-for i in range(10, ROWS + COLS, 10):
-    create_path(matrix, i + random.randint(0, 50))
+num_base_paths = rows + cols // 10
+for i in range(num_base_paths):
+    start_pos = (random.randint(0, rows - 1), random.randint(0, cols - 1))
+    while start_pos in visited_positions:
+        start_pos = (random.randint(0, rows - 1), random.randint(0, cols - 1))
+    create_path(matrix, start_pos, i + random.randint(0, 50))
+
+for i in range(num_base_paths * 2):
+    start_pos = random.choice(visited_positions)
+    create_path(matrix, start_pos, i + random.randint(0, 50), continue_path=True)
 
 print_matrix(highlighted_matrix, cell_width=6)
+with open(output_filename, 'w') as f:
+    for row in matrix:
+        f.write(' '.join(str(cell) for cell in row) + '\n')
