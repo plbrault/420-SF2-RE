e812b9c6a92bec37a8a8cfdd7694d9c9a4db4e54
Pier-Luc Brault <pier-luc@brault.me>
2025-04-01 21:13:02 -0400

    Test de JSON pour obtenir un map

--- laboratoires/lab08-map-json/test-json/Project1/main.cpp
+++ laboratoires/lab08-map-json/test-json/Project1/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <string>
 #include "JSONParser.h"
 
 using namespace std;
@@ -10,4 +12,7 @@ int main() {
     jsonParser.parse(cin);
 
     cout << "Parsed JSON: " << jsonParser.getData().dump() << endl;
+
+    map<string, string> m = jsonParser.getData().template get<map<string, string>>();
+    cout << m["allo"] << endl;
 }
