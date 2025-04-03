bf1b03291ff6abe8a18a25885f281f296092bbba
Pier-Luc Brault <pier-luc@brault.me>
2025-04-03 11:07:40 -0400

    Test concluant set de mots

--- laboratoires/lab08-map-json/tests-json/main.cpp
+++ laboratoires/lab08-map-json/tests-json/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
+#include <set>
 #include "JSONParser.h"
 
 using namespace std;
@@ -8,11 +10,25 @@ using namespace std;
 int main() {
     JSONParser jsonParser;
 
-    cout << "Enter JSON string: ";
-    jsonParser.parse(cin);
+    //cout << "Enter JSON string: ";
+    //jsonParser.parse(cin);
 
-    cout << "Parsed JSON: " << jsonParser.getData().dump() << endl;
+    //cout << "Parsed JSON: " << jsonParser.getData().dump() << endl;
 
-    map<string, string> m = jsonParser.getData().template get<map<string, string>>();
-    cout << m["allo"] << endl;
+    //map<string, string> m = jsonParser.getData().template get<map<string, string>>();
+    //cout << m["allo"] << endl;
+
+    ifstream mots("mots/mots.json");
+    if (!mots.is_open()) {
+        cerr << "Error opening file" << endl;
+        return 1;
+    }
+
+    jsonParser.parse(mots);
+
+    set<string> motsSet = jsonParser.getData().template get<set<string>>();
+
+    if (motsSet.contains("structures")) {
+        cout << "Le mot structures est dans le set" << endl;
+    }
 }
