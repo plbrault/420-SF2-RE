a1fef05c1c861009d368d232a4fdcfa385f55c77
Pier-Luc Brault <pier-luc@brault.me>
2025-04-04 17:20:00 -0400

    WIP

--- laboratoires/lab08a-set-map/solution/DechiffreurFrequence.cpp
+++ laboratoires/lab08a-set-map/solution/DechiffreurFrequence.cpp
@@ -13,9 +13,11 @@ DechiffreurFrequence::DechiffreurFrequence(const std::map<float, std::vector<cha
 
 void DechiffreurFrequence::_compterLettres() {
     _nombreLettres = 0;
-    for (char lettre : _texteChiffre) {
-        if ('a' <= tolower(lettre) <= 'z') {
-            _occurencesLettres[tolower(lettre)]++;
+    for (auto caractere : _texteChiffre) {
+        if ('a' <= tolower(caractere) && tolower(caractere) <= 'z') {
+            _occurencesLettres[tolower(caractere)]++;
+        }
+        if (caractere != '\n') {
             _nombreLettres++;
         }
     }
@@ -29,11 +31,20 @@ void DechiffreurFrequence::dechiffrer() {
             int occ = _occurencesLettres[tolower(caractere)];
             float frequence = (float)occ / (float)_nombreLettres;
             //float frequence = (float)_occurencesLettres[caractere] / (float)_nombreLettres;
+
+            auto itPlusProche = _lettresParFrequenceLangue->begin();
             auto itLowerBound = _lettresParFrequenceLangue->lower_bound(frequence);
+            auto itPrecedent = std::prev(itLowerBound);
             if (itLowerBound == _lettresParFrequenceLangue->end()) {
-                itLowerBound--;
+                itPlusProche = itPrecedent;
+            } else if (itLowerBound == _lettresParFrequenceLangue->begin()) {
+                itPlusProche = itLowerBound;
+            } else {
+                float diff1 = abs(itLowerBound->first - frequence);
+                float diff2 = abs(itPrecedent->first - frequence);
+                itPlusProche = (diff1 < diff2) ? itLowerBound : itPrecedent;
             }
-            fluxTexteDechiffre << itLowerBound->second[0];
+            fluxTexteDechiffre << itPlusProche->second[0];
         } else {
             fluxTexteDechiffre << caractere;
         }
