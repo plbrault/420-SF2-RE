435705e2772d621ffeb7b4376b54b5af9eb1f0ff
Pier-Luc Brault <pier-luc@brault.me>
2025-04-04 20:54:34 -0400

    Nettoyage du main

--- laboratoires/lab08a-set-map/solution/main.cpp
+++ laboratoires/lab08a-set-map/solution/main.cpp
@@ -10,41 +10,13 @@
 using namespace std;
 
 int main() {
-    //JSONParser jsonParser;
-    //ifstream fluxLangueFrancaise("mots.json");
-    //ifstream fluxFrequencesLettres("frequences_lettres.json");
     ifstream fluxTexteChiffre;
-    //set<string> motsLangueFrancaise;
-    //map<float, vector<char>> lettresParFrequence;
 
     Langue francais("mots.json", "frequences_lettres.json");
     francais.charger();
 
     DechiffreurCesar dechiffreurCesar(&francais);
     DechiffreurFrequence dechiffreurFrequence(&francais);
-
-    // Chargement des mots de la langue française
-    /*if (!fluxLangueFrancaise) {
-        cerr << "Erreur d'ouverture du fichier" << endl;
-        return 1;
-    }
-    jsonParser.parse(fluxLangueFrancaise);
-    fluxLangueFrancaise.close();
-    motsLangueFrancaise = jsonParser.getData().template get<set<string>>();*/
-
-    // Chargement des lettres par fréquence
-    /*if (!fluxFrequencesLettres) {
-        cerr << "Erreur d'ouverture du fichier" << endl;
-        return 1;
-    }
-    jsonParser.parse(fluxFrequencesLettres);
-    fluxFrequencesLettres.close();
-    for (const auto& [frequence, lettres] : jsonParser.getData().template get<map<string, vector<string>>>()) {
-        float frequenceFloat = stof(frequence);
-        for (const auto& lettre : lettres) {
-            lettresParFrequence[frequenceFloat].push_back(lettre[0]);
-        }
-    }*/
 
     // Lecture du premier texte à déchiffrer
     fluxTexteChiffre.open("texte1.txt");
