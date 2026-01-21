8d11aed7c47675fb12dca1d436539ce3c10846a5
Pier-Luc Brault <pier-luc@brault.me>
2026-01-21 17:22:57 -0500

    Laboratoire 01-A

---
+++ laboratoires/lab01-pointeurs/solution/lab01b/fonctions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void demanderNombres(float nombres[], int indexMin, int indexMax);
+float calculerMoyenne(float nombres[], int nombreValeurs);
+void afficherMoyenne(int nombreValeurs, float moyenne);
---
+++ laboratoires/lab01-pointeurs/solution/lab01b/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "fonctions.h"
+
+using namespace std;
+
+int main() {
+	float* nombres = nullptr;
+	int nombreValeurs;
+
+	cout << "Combien de nombres voulez-vous entrer? ";
+	cin >> nombreValeurs;
+
+	if (nombreValeurs > 0) {
+		nombres = new float[nombreValeurs];
+		demanderNombres(nombres, 0, nombreValeurs - 1);
+		afficherMoyenne(nombreValeurs, calculerMoyenne(nombres, nombreValeurs));
+	}
+
+	cout << "Voulez-vous ajouter d'autres nombres? (o/n) ";
+	char reponse;
+	cin >> reponse;
+	if (toupper(reponse) == 'O') {
+		int nombreNouvellesValeurs;
+
+		cout << "Combien de nouveaux nombres voulez-vous ajouter? ";
+		cin >> nombreNouvellesValeurs;
+
+		if (nombreNouvellesValeurs > 0) {
+			float* nouveauTableau = new float[nombreValeurs + nombreNouvellesValeurs];
+			for (int i = 0; i < nombreValeurs; i++) {
+				nouveauTableau[i] = nombres[i];
+			}
+			delete[] nombres;
+			nombres = nouveauTableau;
+
+			demanderNombres(nombres, nombreValeurs, nombreValeurs + nombreNouvellesValeurs - 1);
+			nombreValeurs = nombreValeurs + nombreNouvellesValeurs;
+			afficherMoyenne(nombreValeurs, calculerMoyenne(nombres, nombreValeurs));
+		}
+	}
+
+	delete[] nombres;
+
+	// Allocation d'un tableau de 5 éléments
+	int* monTableau = new int[5];
+
+	// Insertion de données dans monTableau
+	for (int i = 0; i < 5; i++) {
+		monTableau[i] = i * i;
+	}
+}
