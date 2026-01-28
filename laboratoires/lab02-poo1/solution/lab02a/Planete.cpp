4fc75dc2146785852fca35f4df1678dd1c510421
Pier-Luc Brault <pier-luc@brault.me>
2026-01-28 15:59:28 -0500

    Classe Planète

---
+++ laboratoires/lab02-poo1/solution/lab02a/Planete.cpp
@@ -0,0 +1,38 @@
+//
+// Created by plbrault on 2026-01-28.
+//
+
+#include "Planete.h"
+
+const double CONSTANTE_GRAVITATIONNELLE = 6.67430e-11;
+
+Planete::Planete() {
+    this->_nom = "(Planète inconnue)";
+    this->_rayon = 0;
+    this->_masse = 0;
+    this->_distanceSoleil = 0;
+}
+
+Planete::Planete(std::string nom, double rayon, double masse, double distanceSoleil) {
+    this->_nom = nom;
+    this->_rayon = rayon;
+    this->_masse = masse;
+    this->_distanceSoleil = distanceSoleil;
+}
+
+void Planete::afficher(std::ostream &sortie) {
+    sortie << "Planète " << this->_nom << ":" << std::endl
+        << " - Rayon: " << this->_rayon << " m" << std::endl
+        << " - Masse: " << this->_masse << " kg" << std::endl
+        << " - Distance du soleil: " << this->_distanceSoleil << std::endl
+        << " - Gravité de surface: " << this->calculerGraviteSurface() << std::endl;
+}
+
+void Planete::afficher() {
+    this->afficher(std::cout);
+}
+
+double Planete::calculerGraviteSurface() {
+    double graviteSurface = CONSTANTE_GRAVITATIONNELLE * this->_masse / (this->_rayon * this->_rayon);
+    return graviteSurface;
+}
---
+++ laboratoires/lab02-poo1/solution/lab02a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Planete.h"
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    Planete terre("Terre", 6371000, 5.97e24, 1.5e8);
+
+    terre.afficher();
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
