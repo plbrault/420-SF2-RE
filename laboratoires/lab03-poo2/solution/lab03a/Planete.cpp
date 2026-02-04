edac2b70aa6d0d03199a5bc20475b60a4f516d04
Pier-Luc Brault <pier-luc@brault.me>
2026-02-04 11:55:51 -0500

    Début solution lab03a

---
+++ laboratoires/lab03-poo2/solution/lab03a/Planete.cpp
@@ -0,0 +1,38 @@
+//
+// Created by plbrault on 2026-01-28.
+//
+
+#include "Planete.h"
+
+const double CONSTANTE_GRAVITATIONNELLE = 6.67430e-11;
+
+Planete::Planete() {
+    this->_nom = "(Planète sans nom)";
+    this->_rayon = 0;
+    this->_masse = 0;
+    this->_distanceEtoile = 0;
+}
+
+Planete::Planete(std::string nom, double rayon, double masse, double distanceEtoile) {
+    this->_nom = nom;
+    this->_rayon = rayon;
+    this->_masse = masse;
+    this->_distanceEtoile = distanceEtoile;
+}
+
+void Planete::afficher(std::ostream &sortie) {
+    sortie << "Planète " << this->_nom << ":" << std::endl
+        << " - Rayon: " << this->_rayon << " m" << std::endl
+        << " - Masse: " << this->_masse << " kg" << std::endl
+        << " - Distance de l'étoile: " << this->_distanceEtoile << std::endl
+        << " - Gravité de surface: " << this->getGraviteSurface() << " m/s^2" << std::endl;
+}
+
+void Planete::afficher() {
+    this->afficher(std::cout);
+}
+
+double Planete::getGraviteSurface() {
+    double graviteSurface = CONSTANTE_GRAVITATIONNELLE * this->_masse / (this->_rayon * this->_rayon);
+    return graviteSurface;
+}
