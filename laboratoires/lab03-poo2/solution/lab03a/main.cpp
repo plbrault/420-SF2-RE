68d736571bbc9c56c2e1082b9d65335c798ada6e
Pier-Luc Brault <pier-luc@brault.me>
2026-02-04 12:13:33 -0500

    Utiliser setters dans le main

--- laboratoires/lab03-poo2/solution/lab03a/main.cpp
+++ laboratoires/lab03-poo2/solution/lab03a/main.cpp
@@ -4,25 +4,47 @@
 const size_t NOMBRE_PLANETES = 8;
 
 int main() {
-    Planete mercure("Mercure", 2439000, 3.301e23, 5.79e7);
-    Planete venus("Vénus", 6052000, 4.867e24, 1.082e8);
-    Planete terre("Terre", 6371000, 5.972e24, 1.496e8);
-    Planete mars("Mars", 3390000, 6.39e23, 2.279e8);
-    Planete jupiter("Jupiter", 69911000, 1.898e27, 7.785e8);
-    Planete saturne("Saturne", 58232000, 5.683e26, 1.433e9);
-    Planete uranus("Uranus", 25362000, 8.681e25, 2.877e9);
-    Planete neptune("Neptune", 24622000, 1.024e26, 4.503e9);
-
     Planete systemeSolaire[NOMBRE_PLANETES];
 
-    systemeSolaire[0] = mercure;
-    systemeSolaire[1] = venus;
-    systemeSolaire[2] = terre;
-    systemeSolaire[3] = mars;
-    systemeSolaire[4] = jupiter;
-    systemeSolaire[5] = saturne;
-    systemeSolaire[6] = uranus;
-    systemeSolaire[7] = neptune;
+    systemeSolaire[0].setNom("Mercure");
+    systemeSolaire[0].setRayon(2439000);
+    systemeSolaire[0].setMasse(3.301e23);
+    systemeSolaire[0].setDistanceEtoile(5.79e7);
+
+    systemeSolaire[1].setNom("Vénus");
+    systemeSolaire[1].setRayon(6052000);
+    systemeSolaire[1].setMasse(4.867e24);
+    systemeSolaire[1].setDistanceEtoile(1.082e8);
+
+    systemeSolaire[2].setNom("Terre");
+    systemeSolaire[2].setRayon(6371000);
+    systemeSolaire[2].setMasse(5.972e24);
+    systemeSolaire[2].setDistanceEtoile(1.496e8);
+
+    systemeSolaire[3].setNom("Mars");
+    systemeSolaire[3].setRayon(3390000);
+    systemeSolaire[3].setMasse(6.39e23);
+    systemeSolaire[3].setDistanceEtoile(2.279e8);
+
+    systemeSolaire[4].setNom("Jupiter");
+    systemeSolaire[4].setRayon(69911000);
+    systemeSolaire[4].setMasse(1.898e27);
+    systemeSolaire[4].setDistanceEtoile(7.785e8);
+
+    systemeSolaire[5].setNom("Saturne");
+    systemeSolaire[5].setRayon(58232000);
+    systemeSolaire[5].setMasse(5.683e26);
+    systemeSolaire[5].setDistanceEtoile(1.433e9);
+
+    systemeSolaire[6].setNom("Uranus");
+    systemeSolaire[6].setRayon(25362000);
+    systemeSolaire[6].setMasse(8.681e25);
+    systemeSolaire[6].setDistanceEtoile(2.877e9);
+
+    systemeSolaire[7].setNom("Neptune");
+    systemeSolaire[7].setRayon(24622000);
+    systemeSolaire[7].setMasse(1.024e26);
+    systemeSolaire[7].setDistanceEtoile(4.503e9);
 
     std::cout << "=== PLANÈTES DU SYSTÈME SOLAIRE ===" << std::endl
               << "===================================" << std::endl << std::endl;
