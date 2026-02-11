fdad0b72bdf47fcbb0ee1d592ca92929a5c5a23b
Pier-Luc Brault <pier-luc@brault.me>
2026-02-11 17:04:26 -0500

    Classe Date complétée

--- laboratoires/lab04-poo3/solution/main.cpp
+++ laboratoires/lab04-poo3/solution/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include "Time.h"
 #include "Duration.h"
+#include "Date.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    Time t(16, 1, 0);
-    t -= Duration(1, 30, 0);
-    std::cout << t << std::endl; // Affiche 16:31:
-
-    return 0;
+    Date dateNaissance(1992, 9, 4);
+    std::cout << dateNaissance;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
