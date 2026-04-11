3d83e1eb7f828aebdf531cf1df3ab6661cd1d92b
Pier-Luc Brault <pier-luc@brault.me>
2026-04-11 19:14:45 -0400

    Architecture de base

--- laboratoires/lab09-file-pile/solution/lab09/GlissadeEau.cpp
+++ laboratoires/lab09-file-pile/solution/lab09/GlissadeEau.cpp
@@ -5,8 +5,9 @@
 #include "GlissadeEau.h"
 #include <iomanip>
 
-#define LARGEUR_LIBELLE 50
-#define LARGEUR_VALEUR 9
+#define LARGEUR_LIBELLE 47
+#define LARGEUR_VALEUR 8
+#define LARGEUR_LIGNE (LARGEUR_LIBELLE + LARGEUR_VALEUR + 2)
 
 GlissadeEau::GlissadeEau() {
     _temps.setTotalSeconds(0);
@@ -21,18 +22,18 @@ void GlissadeEau::mettreAJour(const Duration& incrementTemps) {
 }
 
 void GlissadeEau::afficher(std::ostream& sortie) const {
-    sortie << std::string(20, '*') << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Temps: " <<  this->_temps << " *" << std::endl;
+    sortie << std::string(LARGEUR_LIGNE, '*') << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Temps: " << std::setw(LARGEUR_VALEUR) << std::right << this->_temps << " *" << std::endl;
     sortie << std::setw(LARGEUR_LIBELLE)
-        << std::left << "* Nombre de visiteurs dans la file d'entrée: "
+        << std::left << "* Nombre de visiteurs dans la file d'entree: "
         << std::setw(LARGEUR_VALEUR) << std::right << this->_fileEntree.size() << " *" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de tubes disponibles:" << std::setw(LARGEUR_VALEUR) << std::right << this->_tubesDisponibles.size() << "*" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans la file de montée:" << std::setw(LARGEUR_VALEUR) << std::right << this->_fileMontee.size() << " *" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans le toboggan:" << std::setw(LARGEUR_VALEUR) << std::right << this->_toboggan.getNombreGlisseurs() << "*" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans la zone d'arrivée:" << std::setw(LARGEUR_VALEUR) << std::right << this->_zoneArrivee.size() << " *" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de tubes dans le dépôt:" << std::setw(LARGEUR_VALEUR) << std::right << this->_depotTubes.size() << "*" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans la file de sortie:" << std::setw(LARGEUR_VALEUR) << std::right << this->_fileSortie.size() << "*" << std::endl;
-    sortie << std::setw(LARGEUR_LIBELLE) << std::left << std::string(20, '*') << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de tubes disponibles: " << std::setw(LARGEUR_VALEUR) << std::right << this->_tubesDisponibles.size() << " *" << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans la file de montee: " << std::setw(LARGEUR_VALEUR) << std::right << this->_fileMontee.size() << " *" << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans le toboggan: " << std::setw(LARGEUR_VALEUR) << std::right << this->_toboggan.getNombreGlisseurs() << " *" << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans la zone d'arrivee: " << std::setw(LARGEUR_VALEUR) << std::right << this->_zoneArrivee.size() << " *" << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de tubes dans le depot: " << std::setw(LARGEUR_VALEUR) << std::right << this->_depotTubes.size() << " *" << std::endl;
+    sortie << std::setw(LARGEUR_LIBELLE) << std::left << "* Nombre de visiteurs dans la file de sortie: " << std::setw(LARGEUR_VALEUR) << std::right << this->_fileSortie.size() << " *" << std::endl;
+    sortie << std::string(LARGEUR_LIGNE, '*') << std::endl;
 }
 
 std::ostream& operator<<(std::ostream& os, const GlissadeEau& glissade) {
